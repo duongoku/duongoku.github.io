5fe6afd74e74177aa3f9d50d54997e6aeeb8438e
duongoku <nguyenvubinhduong@gmail.com>
2024-04-27 09:22:07 +0700

    Make (the center of) Mimikyu avoid mouse

--- index.js
+++ index.js
@@ -59,3 +59,36 @@ function gameLoop() {
         window.requestAnimationFrame(gameLoop);
     }, 1000 / 60);
 }
+
+const MIN_DISTANCE_TO_MOUSE = 20;
+function handleMouseMove(event) {
+    var eventDoc, doc, body;
+
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+            (doc && doc.scrollTop || body && body.scrollTop || 0) -
+            (doc && doc.clientTop || body && body.clientTop || 0);
+    }
+
+    let center_x = currentPosition[0] + SCALED_WIDTH / 2;
+    let center_y = currentPosition[1] + SCALED_HEIGHT / 2;
+    let mouse_x = event.pageX;
+    let mouse_y = event.pageY;
+    let distance = Math.sqrt((mouse_x - center_x) * (mouse_x - center_x) + (mouse_y - center_y) * (mouse_y - center_y))
+    let min_distance = MIN_DISTANCE_TO_MOUSE;
+    if (distance < min_distance) {
+        let move_x = (min_distance / distance - 1) * (center_x - mouse_x);
+        let move_y = (min_distance / distance - 1) * (center_y - mouse_y);
+        currentPosition[0] += move_x;
+        currentPosition[1] += move_y;
+        console.log(`${currentPosition}`);
+    }
+}
+document.onmousemove = handleMouseMove;
